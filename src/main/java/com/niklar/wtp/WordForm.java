1053c207f7746ede5fbd683b325aedaef6c9eb40
oskaralfons <art@niklar.com>
2014-09-13 17:57:50 +0200

    Model for a WordForm

---
+++ src/main/java/com/niklar/wtp/WordForm.java
@@ -0,0 +1,18 @@
+package com.niklar.wtp;
+
+/**
+ * Created by Oskar on 13.09.2014.
+ */
+
+public class WordForm {
+
+    public  final String wordForm;
+    //TODO add grammatical
+    public WordForm(String wordForm) {
+        this.wordForm = wordForm;
+    }
+
+    public String getWordForm() {
+        return wordForm;
+    }
+}
