3605b60ced9f5dc91ac129a2210d11a7c324eca9
oskaralfons <art@niklar.com>
2014-09-14 16:46:37 +0200

    added resolvers for simple infinitives

--- src/main/java/com/niklar/wtp/DEVerbWeakInseparableResolver.java
+++ src/main/java/com/niklar/wtp/DEVerbWeakInseparableResolver.java
@@ -1,7 +1,10 @@
 package com.niklar.wtp;
 
+import com.google.common.collect.Sets;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Properties;
 
 /**
@@ -15,9 +18,35 @@ public class DEVerbWeakInseparableResolver {
         return conjugations;
     }
 
-    private static Collection<WordForm> infinitives(final Properties props) {
-        ArrayList<WordForm> infinitives = new ArrayList<WordForm>();
-
+    public static Collection<WordForm> infinitives(final Properties props) {
+        final ArrayList<WordForm> infinitives = new ArrayList<WordForm>();
+        final String prefix = props.getProperty("1", "");
+        // Infinitiv Präsens Aktiv
+        infinitives.add(new WordForm(getBaseForm(props)));
+        // Infinitiv Perfekt Aktiv
+        infinitives.add(new WordForm(prefix + props.getProperty("5", "")));
+        // Infinitiv Präsens Aktiv Partizip+
+        if (!"".equals(props.getProperty("Partizip+", ""))) {
+            infinitives.add(new WordForm(prefix + props.getProperty("Partizip+", "")));
+        }
+        //  Vorgangspassiv / Zusstandspassiv
+        String vp = props.getProperty("vp", "");
+        final String zp = props.getProperty("zp","");
+        HashSet<String> vpZpYes = Sets.newHashSet("ja", "1", "sie_werden", "vp3", "zp3");
+        if (!"".equals(vp)){
+            if (vpZpYes.contains(vp)){
+                // Vorgangspassiv Infinitiv Präsens
+                infinitives.add(new WordForm(prefix+props.get("5")));
+                // Vorgangspassiv Infinitiv Perfekt
+                infinitives.add(new WordForm(prefix+props.get("5")));
+            }
+            if (vpZpYes.contains(zp)){
+                // Zustandspassiv Infinitiv Präsens
+                infinitives.add(new WordForm(prefix+props.get("5")));
+                // Zustandspassiv Infinitiv Perfekt
+                infinitives.add(new WordForm(prefix+props.get("5")));
+            }
+        }
         return infinitives;
     }
 
@@ -27,10 +56,12 @@ public class DEVerbWeakInseparableResolver {
             return prefix + props.getProperty("Infinitiv Präsens");
         } else if (!props.getProperty("10", "").isEmpty()) {
             return prefix + props.getProperty("10");
-        } else if (!props.getProperty("2", "").isEmpty()){
-            return prefix + props.getProperty("2")+"en";
+        } else if (!props.getProperty("2", "").isEmpty()) {
+            return prefix + props.getProperty("2") + "en";
         }
         //TODO throw exception
         return "";
     }
+
+
 }
--- src/test/java/com/niklar/wtp/DEVerbWeakInseparableResolverTest.java
+++ src/test/java/com/niklar/wtp/DEVerbWeakInseparableResolverTest.java
@@ -1,41 +1,70 @@
 package com.niklar.wtp;
 
+import com.google.common.collect.Sets;
 import org.testng.annotations.Test;
+
+import java.util.Collection;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static org.testng.Assert.*;
 
 public class DEVerbWeakInseparableResolverTest {
+    private final String kennenTemplate = "{{Deutsch Verb unregelmäßig|2=kenn|3=kannte|4=kennt|5=gekannt|8=n|vp=ja|zp=nein|gerund=ja}}";
+    private final String seinTemplate = "{{Deutsch Verb unregelmäßig|2=sei|3=war|4=wär|5=gewesen|6=bi|Hilfsverb=sein|vp=nein|zp=nein|gerund=nein\n" +
+            "|Infinitiv Präsens=sein\n" +
+            "|Imperativ (du)=sei\n" +
+            "|Imperativ (ihr)=seid\n" +
+            "|Indikativ Präsens (ich)=bin\n" +
+            "|Indikativ Präsens (du)=bist\n" +
+            "|Indikativ Präsens (man)=ist\n" +
+            "|Indikativ Präsens (wir)=sind\n" +
+            "|Indikativ Präsens (ihr)=seid\n" +
+            "|Indikativ Präsens (sie)=sind\n" +
+            "|Konjunktiv Präsens (ich)=sei\n" +
+            "|Konjunktiv Präsens (du)=seiest\n" +
+            "|Konjunktiv Präsens Alternativform (du)=seist\n" +
+            "|Konjunktiv Präsens (man)=sei\n" +
+            "|Konjunktiv Präsens (wir)=seien\n" +
+            "|Konjunktiv Präsens (ihr)=seiet\n" +
+            "|Konjunktiv Präsens (sie)=seien}}\n";
+    private final String antunTemplate = "{{Deutsch Verb unregelmäßig|1=an|2=tu|3=tat|4=tät|5=getan|10=tun|vp=ja|zp=ja|gerund=ja}}";
+    private final Properties kennenProps = ParsingUtils.parseRule(kennenTemplate, "Deutsch Verb unregelmäßig", 0);
+    private final Properties seinProps = ParsingUtils.parseRule(seinTemplate, "Deutsch Verb unregelmäßig", 0);
+    private final Properties antunProps = ParsingUtils.parseRule(antunTemplate, "Deutsch Verb unregelmäßig", 0);
+
+
     @Test
     public void testBaseForm() throws Exception {
-        final String kennenTemplate = "{{Deutsch Verb unregelmäßig|2=kenn|3=kannte|4=kennt|5=gekannt|8=n|vp=ja|zp=nein|gerund=ja}}";
-        final String seinTemplate = "{{Deutsch Verb unregelmäßig|2=sei|3=war|4=wär|5=gewesen|6=bi|Hilfsverb=sein|vp=nein|zp=nein|gerund=nein\n" +
-                "|Infinitiv Präsens=sein\n" +
-                "|Imperativ (du)=sei\n" +
-                "|Imperativ (ihr)=seid\n" +
-                "|Indikativ Präsens (ich)=bin\n" +
-                "|Indikativ Präsens (du)=bist\n" +
-                "|Indikativ Präsens (man)=ist\n" +
-                "|Indikativ Präsens (wir)=sind\n" +
-                "|Indikativ Präsens (ihr)=seid\n" +
-                "|Indikativ Präsens (sie)=sind\n" +
-                "|Konjunktiv Präsens (ich)=sei\n" +
-                "|Konjunktiv Präsens (du)=seiest\n" +
-                "|Konjunktiv Präsens Alternativform (du)=seist\n" +
-                "|Konjunktiv Präsens (man)=sei\n" +
-                "|Konjunktiv Präsens (wir)=seien\n" +
-                "|Konjunktiv Präsens (ihr)=seiet\n" +
-                "|Konjunktiv Präsens (sie)=seien}}\n";
-        final String antunTemplate = "{{Deutsch Verb unregelmäßig|1=an|2=tu|3=tat|4=tät|5=getan|10=tun|vp=ja|zp=ja|gerund=ja}}";
-
         assertThat(DEVerbWeakInseparableResolver
-                .getBaseForm(ParsingUtils.parseRule(kennenTemplate, "Deutsch Verb unregelmäßig", 0)))
+                .getBaseForm(kennenProps))
                 .isEqualToIgnoringCase("kennen");
         assertThat(DEVerbWeakInseparableResolver
-                .getBaseForm(ParsingUtils.parseRule(seinTemplate, "Deutsch Verb unregelmäßig", 0)))
+                .getBaseForm(seinProps))
                 .isEqualToIgnoringCase("sein");
         assertThat(DEVerbWeakInseparableResolver
-                .getBaseForm(ParsingUtils.parseRule(antunTemplate, "Deutsch Verb unregelmäßig", 0)))
+                .getBaseForm(antunProps))
                 .isEqualToIgnoringCase("antun");
+    }
+    @Test
+    public void testInfinitives() {
+        Set<String> kennenInfinitives = Sets.newHashSet("kennen", "gekannt");
+        Set<String> seinInfinitives = Sets.newHashSet("sein", "gewesen");
+        Set<String> antunInfinitives = Sets.newHashSet("antun", "angetan");
+
+        assertThat(wordFormToStrings(DEVerbWeakInseparableResolver.infinitives(kennenProps)))
+                .isEqualTo(kennenInfinitives);
+        assertThat(wordFormToStrings(DEVerbWeakInseparableResolver.infinitives(seinProps)))
+                .isEqualTo(seinInfinitives);
+        assertThat(wordFormToStrings(DEVerbWeakInseparableResolver.infinitives(antunProps)))
+                .isEqualTo(antunInfinitives);
+    }
+
+    private Set<String> wordFormToStrings(final Collection<WordForm> wordForms) {
+        return wordForms.stream()
+                .map(WordForm::getWordForm)
+                .collect(Collectors.toSet());
     }
 }
